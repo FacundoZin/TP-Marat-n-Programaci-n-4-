116ebc199eaf2fb49aa5261d9f3a3b3952122929
Facu Zin <facundozin10@gmail.com>
2025-10-04 11:10:42 -0300

    installing dependencies

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,8 @@
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
 import App from './App.vue'
 import router from './router'
 
